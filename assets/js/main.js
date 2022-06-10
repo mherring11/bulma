d772d418862cd6b6edf7f8ffe1eec3c11e5a333d
SEGuerra <sameguerra@sbcglobal.net>
2022-06-10 17:59:33 -0500

    adding pseudocode for my Search functionality to be pushed to my feature branch

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/***************************************/
+/**PSEUDOCODE FOR BUTTON FUNCTIONALITY**/
+/***************************************/
+
+// When the user enters the information in the form
+// and clicks the button
+// the APIs get called and stuff gets displayed on the page
+// the search criteria gets stored in localStorage
+// and a card is displayed on the page in the Past Searches area
