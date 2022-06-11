df6051c2d3ba96f9d9232c8795a849a62241c313
SEGuerra <sameguerra@sbcglobal.net>
2022-06-10 22:07:50 -0500

    grabs input info and starts building and object/saves to localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,44 @@
 // the APIs get called and stuff gets displayed on the page
 // the search criteria gets stored in localStorage
 // and a card is displayed on the page in the Past Searches area
+
+// Global variables
+let formEl = document.querySelector("#search-form");
+let pastSearch = document.querySelector("#past-search");
+let searchBtn = document.querySelector("#search-btn");
+
+let searchInfo = [];
+
+let searchFormHandler = function(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("input[id='searched-location']").value;
+    
+  console.log(searchInput);
+
+  // checking if there is a valid input
+    if (!searchInput) {
+      alert("Please fill in a destination.")        
+      return false;
+    }
+    
+  // reset form for next search
+  document.querySelector("input[id='searched-location']").value = "";
+    
+  // create object to pass to past searches and save function
+  let searchInputObj = {
+      city: searchInput
+  }  
+  
+  searchInfo.push(searchInputObj);
+  saveInfo();
+}
+
+// save input as an object in localStorage
+let saveInfo = function() {
+    localStorage.setItem("searchInfo", JSON.stringify(searchInfo));
+}
+
+
+
+// search event listener
+formEl.addEventListener("submit", searchFormHandler);
